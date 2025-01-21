d1bbb47d2705306b3cdaa5e0d96cf636e8344ef2
otaciliow <otacilio.dml@hotmail.com>
2025-01-21 10:14:45 -0300

    feat: added cart items manipulation (+)

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -4,6 +4,7 @@ import { IProductsProps } from '../interfaces/productsProps.interface';
 interface ICartContextData {
     cart: ICartProps[];
     cartAmount: number;
+    addItemCart: (newItem: IProductsProps) => void;
 }
 
 interface ICartProps extends IProductsProps {
@@ -19,8 +20,32 @@ export const CartContext = createContext({} as ICartContextData);
 
 function CartProvider({ children }: ICartProviderProps) {
     const [cart, setCart] = useState<ICartProps[]>([]);
+
+    function addItemCart(newItem: IProductsProps) {
+        const indexItem  = cart.findIndex(item => item.id === newItem.id);
+
+        if (indexItem !== -1) {
+            let cartList = cart;
+
+            cartList[indexItem].amount = cartList[indexItem].amount + 1;
+            cartList[indexItem].total = cartList[indexItem].amount * cartList[indexItem].price;
+
+            setCart(cartList);
+
+            return;
+        }
+
+        let data = {
+            ...newItem,
+            amount: 1,
+            total: newItem.price
+        }
+
+        setCart(products => [...products, data]);
+    }
+
     return(
-        <CartContext.Provider value={{ cart, cartAmount: cart.length }}>
+        <CartContext.Provider value={{ cart, cartAmount: cart.length, addItemCart }}>
             {children}
         </CartContext.Provider>
     )
